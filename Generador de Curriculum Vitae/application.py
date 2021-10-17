bfd7431cb558f0eb38deec8f39a5a35e67cf3cfe
Walter Stephano Mejia Morales <walter.mejia@ucsp.edu.pe>
2021-10-16 21:46:48 -0500

    Avance

--- Generador de Curriculum Vitae/application.py
+++ Generador de Curriculum Vitae/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_session import Session
 
 app = Flask(__name__)
@@ -7,47 +7,54 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    username = request.form.get("nombres")
-    password = request.form.get("apellidos")
-
-    return render_template("index.html", notes=session["notes"])
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    correo = request.form.get("correo")
-    username = request.form.get("username")
-    password1 = request.form.get("password1")
-    password2 = request.form.get("password2")
+    return render_template('login.html')
     
-    return render_template("register.html", notes=session["notes"])
+@app.route("/login", methods=["POST"])
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        return render_template("cuestionario.html")
+    else:
+        return "bad request"
+    
 
-@app.route("/cuestionario", methods=["GET", "POST"])
+@app.route("/register", methods=["POST"])
+def register():
+    correo = request.form['correo']
+    username = request.form['username']
+    password1 = request.form['password1']
+    password2 = request.form['password2']
+    
+    return render_template("register.html")
+
+@app.route("/cuestionario", methods=["POST"])
 def cuestionario():
     
-    fullname = request.form.get("fullname")
-    profesion = request.form.get("profesion")
-    correo = request.form.get("correo")
-    number = request.form.get("number")
-    estudios = request.form.get("estudios")
-    habilidades = request.form.get("habilidades")
-    presentacion = request.form.get("presentacion")
-    experiencia = request.form.get("experiencia")
+    fullname = request.form['fullname']
+    profesion = request.form['profesion']
+    correo = request.form['correo']
+    number = request.form['number']
+    estudios = request.form['estudios']
+    habilidades = request.form['habilidades']
+    presentacion = request.form['presentacion']
+    experiencia = request.form['experiencia']
 
-    return render_template("cuestionario.html", notes=session["notes"])
+    return render_template("cuestionario.html")
 
 
-@app.route("/condiciones", methods=["GET", "POST"])
+@app.route("/condiciones", methods=[ "POST"])
 def condiciones():
     return render_template("condiciones.html")
 
-@app.route("/curriculums", methods=["GET", "POST"])
+@app.route("/curriculums", methods=["POST"])
 def curriculums():
     return render_template("curriculums.html")
-@app.route("/generador", methods=["GET", "POST"])
+@app.route("/generador", methods=["POST"])
 def generador():
     return render_template("generador.html")
-@app.route("/plantilla1", methods=["GET", "POST"])
+@app.route("/plantilla1", methods=["POST"])
 def plantilla1():
     return render_template("plantilla1.html")
